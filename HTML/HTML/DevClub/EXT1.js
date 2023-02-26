e4d22f88ee7564aee8793764a2ac87030100770c
igor-soaresb <igorsoaresb07@gmail.com>
2023-02-26 17:27:37 -0300

    Primeiro Commit

---
+++ HTML/HTML/DevClub/EXT1.js
@@ -0,0 +1,9 @@
+const numero1 = 4
+
+const resto = numero 1 % 2
+if(resto==0)
+ {console.log("O numero é par")
+}else{
+console.log("O numero é ímpar")
+}
+
---
+++ Starbucks/scripts.js
@@ -0,0 +1,12 @@
+let imagem = document.querySelector(".starbucks")
+    let circulo = document.querySelector(".circulo")
+
+    function trocaimagem (endereco){
+        imagem.src = endereco
+
+    }
+
+function trocacor (cor){
+circulo.style.background = cor
+
+}
